5a85c52178ea81179bdc778723561b03364c3734
Gustavo <gus.inacio@hotmail.com>
2022-07-06 12:58:07 -0300

    feat: Initial structure of movie cards

--- src/components/HomeCards.vue
+++ src/components/HomeCards.vue
@@ -1,18 +1,26 @@
 <template>
   <v-container>
-    <v-row class="text-center">
-      <v-row justify="space-around">
-        <v-card align="center" class="ma-5" display="flex"
-         justify="center" width="400px" v-for="movie in movies" :key="movie.id">
-          <v-img :src="`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`" />
+    <div class="d-flex justify-center flex-wrap align-center">
+      <v-col class="d-flex justify-center" v-for="movie in movies" :key="movie.id">
+        <v-card
+          align="center"
+          class="d-flex flex-column align-center"
+          width="300px"
+          height="450px"
+        >
+          <v-img
+            :src="`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`"
+          />
           <v-card-title>{{ movie.title }}</v-card-title>
           <v-col>
-            {{ movie.vote_average }}
+            {{ movie.vote_average.toFixed(1) }}
           </v-col>
-          <v-btn width="100%" alignSelf="flex-end" color="primary">Adicionar</v-btn>
+          <v-btn width="100%" color="primary"
+            >Adicionar</v-btn
+          >
         </v-card>
-      </v-row>
-    </v-row>
+      </v-col>
+    </div>
   </v-container>
 </template>
 
