bfa2d3e5db7821ccb0a27ac2bafd556a659f0d55
Gustavo <gus.inacio@hotmail.com>
2022-07-06 16:06:46 -0300

    feat: config store

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,12 +5,26 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    movies: [],
+    cart: [],
   },
   getters: {
   },
   mutations: {
+    getMovies(state, payload) {
+      state.movies = payload;
+    },
+    searchMovies(state, payload) {
+      state.movies = payload;
+    },
   },
   actions: {
+    getMovies(context, payload) {
+      context.commit('getMovies', payload);
+    },
+    searchMovies(context, payload) {
+      context.commit('searchMovies', payload);
+    },
   },
   modules: {
   },
