cddaaca41e071a5e68f735e9cede1ec256a1bc35
Gustavo <gus.inacio@hotmail.com>
2022-07-06 14:39:33 -0300

    feat: Endpoint to fetch movies by query

--- src/services/movies.service.ts
+++ src/services/movies.service.ts
@@ -3,3 +3,5 @@ import { api, apiKey } from '.';
 export const GET_MOVIES = () => api.get(`trending/movie/week?api_key=${apiKey}&language=pt-BR`);
 
 export const GET_MOVIE_BY_ID = (id: number) => api.get(`movie/${id}?api_key=${apiKey}&language=pt-BR`);
+
+export const SEARCH_MOVIE = (query: string) => api.get(`search/movie?api_key${apiKey}&language=pt-BR&query=${query}`);
