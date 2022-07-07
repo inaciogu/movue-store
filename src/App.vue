11744237a58db667c6ef99208c7d74eda557e197
Gustavo <gus.inacio@hotmail.com>
2022-07-07 09:28:52 -0300

    feat: sidebar cart footer

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,38 @@
   <v-app>
     <header-app />
     <v-navigation-drawer bottom width="400" fixed temporary v-model="drawer">
-      <v-card>
-        <div class="d-flex justify-space-between">
-          <v-card-title> Meu Carrinho </v-card-title>
-          <v-card-actions>
-            <v-btn outlined @click="clearCart">Esvaziar carrinho</v-btn>
-          </v-card-actions>
-        </div>
-        <v-list>
-          <v-list-item :key="item.id" v-for="item in cart">
-            <v-list-item-group
-              style="width: 100%"
-              class="d-flex justify-space-between align-center"
-            >
-              <v-list-item-title style="max-width: 50%; bread-word: wrap">
-                {{ item.title }}
-              </v-list-item-title>
-              {{ item.quantity }}
-              <v-btn icon @click="removeItem(item.id)">
-                <v-icon>mdi-delete-outline</v-icon>
-              </v-btn>
-            </v-list-item-group>
-          </v-list-item>
-        </v-list>
+      <v-card
+        class="d-flex flex-column justify-space-between"
+        style="height: 100%"
+      >
+        <v-card-content>
+          <div class="d-flex justify-space-between">
+            <v-card-title> Meu Carrinho </v-card-title>
+            <v-card-actions>
+              <v-btn outlined @click="clearCart">Esvaziar carrinho</v-btn>
+            </v-card-actions>
+          </div>
+          <v-list>
+            <v-list-item :key="item.id" v-for="item in cart">
+              <v-list-item-group
+                style="width: 100%"
+                class="d-flex justify-space-between align-center"
+              >
+                <v-list-item-title style="max-width: 50%; bread-word: wrap">
+                  {{ item.title }}
+                </v-list-item-title>
+                {{ item.quantity }}
+                <v-btn icon @click="removeItem(item.id)">
+                  <v-icon>mdi-delete-outline</v-icon>
+                </v-btn>
+              </v-list-item-group>
+            </v-list-item>
+          </v-list>
+        </v-card-content>
+        <v-footer class="d-flex align-center justify-space-between">
+          <p>Total:</p>
+          <p>19.99</p>
+        </v-footer>
       </v-card>
     </v-navigation-drawer>
     <v-main>
