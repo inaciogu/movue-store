7c22158d88e896f6fba77a230bbfd4399f72336a
Gustavo <gus.inacio@hotmail.com>
2022-07-13 16:31:54 -0300

    fix: remove comments

--- src/router/index.ts
+++ src/router/index.ts
@@ -13,10 +13,7 @@ const routes: Array<RouteConfig> = [
   {
     path: '/purchase',
     name: 'purchase',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/PurchaseView.vue'),
+    component: () => import('../views/PurchaseView.vue'),
   },
 ];
 
